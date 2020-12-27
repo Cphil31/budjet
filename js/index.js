6bd9bd8249d9c1fdef8581d69e6f382ee2bd5ddd
Cphil31 <cecilephilippe31@gmail.com>
2020-12-27 16:53:22 +0100

    delete element

--- js/index.js
+++ js/index.js
@@ -1,3 +1,5 @@
+
+
 window.onload = function () {
 
     let butSub = document.querySelector(".submit");
@@ -6,32 +8,68 @@ window.onload = function () {
     let inputPrice = document.querySelector("input[name ='price']");
     let tabProduct = [] ;
     let divTotal = document.querySelector('.total')
-
+    
     
     let divList = document.querySelector(".list");
     let ul = document.createElement("ul");
+    ul.setAttribute("id","menu");
     divList.appendChild(ul);
-
-
+    
+    
     let total = parseFloat(0) ;
+    let id = parseInt(0);
+    
+    
+    let button = document.createElement("button");
+    button.setAttribute("id","buttonList")
     
     butSub.addEventListener("click",function(e){
         
-        let object = {"product" : `${inputProduct.value}` , "price" : inputPrice.value }
+        id+=1;
         
+        let object = { 
+            "id" : id, 
+            "product" : `${inputProduct.value}` , 
+            "price" : inputPrice.value }
+            
         tabProduct.push(object);
         
+        
         let li = document.createElement("li");
-        li.innerHTML = `${inputProduct.value} :  ${inputPrice.value} €`;
+        li.setAttribute("id",id);
+        
+        
+        li.innerHTML = `${id} : ${inputProduct.value} :  ${inputPrice.value} € `;
         ul.appendChild(li); 
         
+            
         total = total + parseFloat(inputPrice.value) ;
 
 
         divTotal.innerHTML = `Total : ${total.toFixed(2)} €`;
+        
+  
+        
+        button.innerHTML="";
+        button.innerHTML = "supprimer";
+
+        document.body.appendChild(button);
+
+        let buttonList = document.querySelector("#buttonList");
+        let getUl = document.getElementById('menu');
+
+        
+        buttonList.addEventListener("click",function(){
+            /*
+            console.log(ul.lastChild);
+            ul.lastChild.removeChild();
+            */
+           getUl.removeChild(getUl.lastElementChild);
+           
+        })
+
         e.preventDefault();
     })
     
-
-
+    
 }

efeb8feebd82d1b2d62a7845c1dbe523809466b6
Cphil31 <cecilephilippe31@gmail.com>
2021-02-05 15:12:22 +0100

    js arrangé

--- js/index.js
+++ js/index.js
@@ -1,21 +1,23 @@
+
 window.onload = function () {
-
+    
     let butSub = document.querySelector(".submit");
-
+    
     let inputProduct = document.querySelector("input[name ='product']");
     let inputPrice = document.querySelector("input[name ='price']");
     let tabProduct = [];
     let divTotal = document.querySelector('.total');
     let divDelete = document.querySelector(".delete");
-    
-    
     let total = parseFloat(0);
+    
+    
     let id = parseInt(0);
     
     
-    let button = document.createElement("button");
-    button.setAttribute("id", "buttonList")
-    button.setAttribute("onClick","remove()")
+    let buttonDelete = document.createElement("button");
+    buttonDelete.setAttribute("id", "buttonList")
+
+
     butSub.addEventListener("click", function (e) {
         
         id += 1;
@@ -28,14 +30,14 @@ window.onload = function () {
             "product": `${inputProduct.value}`,
             "price": priceValue
         }
-
+        
         tabProduct.push(object);
-
-
+        
+        
         let li = document.createElement("li");
         li.setAttribute("id", id);
-
-
+        
+        
         li.innerHTML = `${id} : ${inputProduct.value} :  ${priceValue.toFixed(2)}  € `;
         menu.appendChild(li);
         
@@ -47,20 +49,35 @@ window.onload = function () {
         
         
         
-        button.innerHTML = "";
-        button.innerHTML = "effacer la liste";
+        buttonDelete.innerHTML = "";
+        buttonDelete.innerHTML = "suprimer produit "
         
-        divDelete.appendChild(button);
-        
-        
-        
-        
-        let buttonList = document.querySelector("#buttonList");
+        divDelete.appendChild(buttonDelete);
+
         
         
-    
         e.preventDefault();
-    })
+    })  // fin de boutton submit
     
     
-}
+    /*
+    */
+    function remove(){
+        menu.removeChild(menu.lastElementChild);
+        let divTotal = document.querySelector(".total");
+        //console.log(total);
+        divTotal.innerHTML="";
+        console.log(divTotal);
+        console.log(total);
+        //total.innerHTML = `Total : ${total.toFixed(2)} €`
+        //total.innerHTML = `Total : ${total.toFixed(2)} €`;
+    }
+    
+    // au click du bouton 
+    buttonDelete.addEventListener("click",function(){
+        console.log("delete button");
+        remove();
+    }) // fin de  click du bouton
+
+    
+} //fin de on load 
